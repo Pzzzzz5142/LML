616663be1e0fc60cf6ac0c344526ce8e055c85ad
译 张 <hello-cd.plus@hotmail.com>
2019-11-21 09:54:37 +0800

    First

---
+++ aa.py
@@ -0,0 +1,26 @@
+class MyTrain():
+    def __init__(self, input_num, activator):
+
+        self.activator = activator
+        self.bias = 0
+        self.weight = [0.0 for i in range(input_num)]
+
+    def __str__(self):
+        '''
+        打印学习到的权重、偏置项
+        '''
+        return 'weights\t:%s\nbias\t:%f\n' % (self.weight, self.bias)
+
+    def predict(self, input_vec):
+        return self.activator([lambda x: x*self.weight+self.bias for x in input_vec])
+
+    def train(self, input_vec, labels, iteration, rate):
+        for i in range(iteration):
+            self.__one__it__(input_vec, labels, rate)
+
+    def __one__it__(self, input_vec, labels, rate):
+        for i, j in zip(input_vec, labels):
+            output = predict(i)
+
+    def update_th(self, input_vec, labels):
+        delta =
