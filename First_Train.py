d7787b774ca4add731d03934c07fedc64defb977
译 张 <hello-cd.plus@hotmail.com>
2019-11-21 16:38:05 +0800

    感知器完成

--- First_Train.py
+++ First_Train.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+# 本文件包含一个感知器
 
 class MyTrain():
     def __init__(self, input_num, activator):
@@ -15,7 +16,15 @@ class MyTrain():
         return 'weights\t:%s\nbias\t:%f\n' % (self.weight, self.bias)
 
     def predict(self, input_vec):  # 根据s输入得到预测值，输入为x向量，输出为单个的值
-        return self.activator(reduce(lambda x, y: x+y, [lambda x: x*self.weight+self.bias for x in input_vec]))
+        su = 0
+        ls = list(map(lambda x: x[0]*x[1]+self.bias,
+                      zip(input_vec, self.weight)))
+        # print(ls)
+        for i in ls:
+            su += i
+        # print(su)
+        return self.activator(su)
+        # return self.activator(reduce(lambda x, y: x+y, [lambda x: x*self.weight+self.bias for x in input_vec]))
 
     def train(self, input_vecs, labels, iteration, rate):  # input_vec是数据集，里面的元素是训练参数向量
         for i in range(iteration):
@@ -26,8 +35,43 @@ class MyTrain():
             pd_output = self.predict(input_vec)
             self.update_par(input_vec, pd_output, j, rate)
 
-    def update_par(self, intput_vec, pred_val, label, rate):
+    def update_par(self, input_vec, pred_val, label, rate):
         delta = label-pred_val
         delta *= rate
         self.bias += delta
-        self.weight
+        self.weight = list(
+            map(lambda x: x[0]+delta*x[1], zip(self.weight, input_vec)))
+
+
+def F(x):
+    '''
+    激活函数
+    '''
+    return 1 if x > 0 else 0
+
+
+def getdata():
+    input_vecs = [[0, 0], [1, 1], [1, 0], [0, 1]]
+    lables = [0, 1, 0, 0]
+    return input_vecs, lables
+
+
+def train_and_perception():
+
+    p = MyTrain(2, F)
+    input_vecs, labels = getdata()
+    p.train(input_vecs, labels, 10, 0.1)
+    return p
+
+
+if __name__ == "__main__":
+    ls = range(10)
+    a = [lambda x: x+10 for x in ls]
+    print(a)
+    xxx = train_and_perception()
+    print(xxx)
+
+    print('1 and 1 = %d' % xxx.predict([1, 1]))
+    print('0 and 0 = %d' % xxx.predict([0, 0]))
+    print('1 and 0 = %d' % xxx.predict([1, 0]))
+    print('0 and 1 = %d' % xxx.predict([0, 1]))
